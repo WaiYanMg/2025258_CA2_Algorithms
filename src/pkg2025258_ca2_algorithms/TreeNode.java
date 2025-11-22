c536416a791b8f178aca2e65ea4342032db8e579
WaiYanMg <waiyanmgw@gmail.com>
2025-11-22 23:32:41 +0000

    add Tree Node container (employee, left, right)

--- src/pkg2025258_ca2_algorithms/TreeNode.java
+++ src/pkg2025258_ca2_algorithms/TreeNode.java
@@ -8,6 +8,22 @@ package pkg2025258_ca2_algorithms;
  *
  * @author Wai
  */
+//TreeNode Class - Represents a node in the binary tree
+//Each node stores an employee and references to left and right children
+//Used for building organizational hierarchy
 public class TreeNode {
+  
+    Employee employee;
+    TreeNode left;
+    TreeNode right;
     
+    /**
+     * Constructor - creates a new tree node with employee data
+     * @param employee The employee to store in this node
+     */
+    public TreeNode(Employee employee) {
+        this.employee = employee;
+        this.left = null;
+        this.right = null;
+    }
 }
