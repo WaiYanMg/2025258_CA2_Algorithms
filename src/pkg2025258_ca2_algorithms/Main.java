8225f59b56c6b816aec6f482c59f4d701fcc45eb
WaiYanMg <waiyanmgw@gmail.com>
2025-11-22 23:09:12 +0000

    Main Console Display testing

--- src/pkg2025258_ca2_algorithms/Main.java
+++ src/pkg2025258_ca2_algorithms/Main.java
@@ -15,6 +15,18 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("\n" + "=".repeat(60));
+        System.out.println("           DEPARTMENT STORE MANAGEMENT SYSTEM");
+        System.out.println("=".repeat(60));
+        System.out.println("1. LOAD DATA FROM FILE");
+        System.out.println("2. SORT EMPLOYEES (Quick Sort)");
+        System.out.println("3. SEARCH EMPLOYEE (Binary Search)");
+        System.out.println("4. ADD NEW EMPLOYEE");
+        System.out.println("5. BUILD ORGANIZATIONAL TREE");
+        System.out.println("6. DISPLAY ALL EMPLOYEES");
+        System.out.println("7. EXIT");
+        System.out.println("=".repeat(60));
+        System.out.print("Please select an option (1-7): ");
     }
     
 }
