c67de20af9e0cf410435bbac88f663229761d560
WaiYanMg <waiyanmgw@gmail.com>
2025-11-28 01:40:50 +0000

    changing package name to CA_2

--- src/CA_2/EmployeeBinaryTree.java
+++ src/CA_2/EmployeeBinaryTree.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package pkg2025258_ca2_algorithms;
+package CA_2;
 
 import java.util.LinkedList;
 import java.util.Queue;
